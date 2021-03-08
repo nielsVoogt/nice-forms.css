a7bfd0b0e9dc82f772893e2c349864f9b8960ca4
niels voogt <n.voogt@gmail.com>
2021-03-08 11:53:46 +0100

    Testing gulp

--- gulpfile.js
+++ gulpfile.js
@@ -6,21 +6,36 @@ const cssnano = require('cssnano');
 
 sass.compiler = require('node-sass');
 
-gulp.task('compile-css:dist', function () {
-    const plugins = [ autoprefixer(), cssnano() ];
+// Paths
 
-    return gulp.src('./nice-forms.scss')
+const libSrc = './nice-forms.scss'
+const libDest = './dist'
+const docsStyleSrc = './docs/scss/*.scss'
+const docsStyleDest = './docs/css'
+
+// PostCSS plugins
+
+const plugins = [
+    autoprefixer(),
+    cssnano()
+];
+
+// ----------- TASKS
+
+gulp.task('compile-docs', function () {
+    return gulp.src(docsStyleSrc)
         .pipe(sass().on('error', sass.logError))
         .pipe(postcss(plugins))
-        .pipe(gulp.dest('./dist'));
+        .pipe(gulp.dest(docsStyleDest));
 });
 
-gulp.task('compile-css:docs', function() {
-    return gulp.src('./docs/scss/*.scss')
+gulp.task('compile-dist', function() {
+    return gulp.src(libSrc)
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./docs/css'));
+        .pipe(postcss(plugins))
+        .pipe(gulp.dest(libDest));
 })
 
 gulp.task('watch', function() {
-    return gulp.watch(['./public/stylesheets/*.scss'], ['sass']);
+    return gulp.watch([docsStyleSrc, libSrc], ['compile-docs']);
 })
